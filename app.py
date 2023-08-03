c0256d5635da1ec76a05fb2ef236a45f70a2fbb3
Malika Mujahid <110493023+malikamujahid@users.noreply.github.com>
2023-08-03 23:18:06 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import json
+import random
+import string
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+shortened_urls = {}      # dictionary
+
+
+def generate_short_url(length=6):
+    chars = string.ascii_letters + string.digits
+    short_url = "".join(random.choice(chars) for _ in range(length))
+    return short_url
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        long_url = request.form['long_url']
+        short_url = generate_short_url()
+        while short_url in shortened_urls:
+            short_url = generate_short_url()
+
+        shortened_urls[short_url] = long_url
+        with open("urls.json", "w") as file:
+            json.dump(shortened_urls, file)
+        return f"Shortened URL: {request.url_root}{short_url}"
+    return render_template("index.html")
+
+@app.route("/<short_url>")
+def redirect_url(short_url):
+    with open("urls.json", "r") as file:
+        shortened_urls = json.load(file)
+    long_url = shortened_urls.get(short_url)
+    if long_url:
+        return redirect(long_url)
+    else:
+        return "URL NOT FOUND", 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
